d58033fcb4d1032d3960c6e5d4e702914b56a158
Maneena Xiong <xiong.maneena@gmail.com>
2021-03-19 13:24:46 -0500

    worked on task 1-3 on week 4 hw assignment part 3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,25 @@
 console.log('***** Cart Functions *****');
+let basket = []
+
+function addItem(item){
+  basket.push(item);
+  return true;
+}
+console.log(addItem('apples'));
+console.log(addItem('chocolate'));
+console.log(basket);
+
+function listItems(){
+  for (let item of basket ){
+    console.log('list of items', item);
+  }
+}
+listItems();
+
+function empty(){
+  basket = [];
+}
+empty();
+console.log('basket is empty', basket);
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
